54c16b951207777431dfd9f4454298763eb77416
Evgenii Mozharovskii <mozharovsky@live.com>
2024-06-24 06:48:33 +0400

    refactor(server): remove dirs methods in file service

--- server/src/services/fileService.ts
+++ server/src/services/fileService.ts
@@ -1,51 +1,18 @@
 import fg from "fast-glob";
 import fs from "fs-extra";
+import type { ListOptions } from "~/models/fs";
 import { unwrapErrorMessage } from "~/utils/zodErrors";
 
-export interface ListOptions {
-  includePatterns?: string[];
-  excludePatterns?: string[];
-  recursive?: boolean;
-}
-
 export interface IFileService {
-  listDirectories(directory: string, options?: ListOptions): Promise<string[]>;
   listFiles(directory: string, options?: ListOptions): Promise<string[]>;
   createFile(filePath: string, content: string): Promise<void>;
   deleteFile(filePath: string): Promise<void>;
   updateFile(filePath: string, content: string): Promise<void>;
   renameFile(oldPath: string, newPath: string): Promise<void>;
   moveFile(oldPath: string, newPath: string): Promise<void>;
-  createDirectory(directoryPath: string): Promise<void>;
-  deleteDirectory(directoryPath: string): Promise<void>;
 }
 
 export class FileService implements IFileService {
-  async listDirectories(directory: string, options: ListOptions = {}): Promise<string[]> {
-    try {
-      const patterns = options.includePatterns ?? ["*"];
-      const ignore = options.excludePatterns ?? [];
-      const directories: string[] = [];
-
-      for (const pattern of patterns) {
-        const entries = await fg(pattern, {
-          cwd: directory,
-          onlyDirectories: true,
-          absolute: true,
-          dot: true,
-          ignore,
-          deep: options.recursive ? Infinity : 1,
-        });
-
-        directories.push(...entries);
-      }
-
-      return directories;
-    } catch (error) {
-      throw new Error(unwrapErrorMessage(error));
-    }
-  }
-
   async listFiles(directory: string, options: ListOptions = {}): Promise<string[]> {
     try {
       const patterns = options.includePatterns ?? ["*"];
@@ -106,22 +73,6 @@ export class FileService implements IFileService {
   async moveFile(oldPath: string, newPath: string): Promise<void> {
     try {
       await fs.move(oldPath, newPath);
-    } catch (error) {
-      throw new Error(unwrapErrorMessage(error));
-    }
-  }
-
-  async createDirectory(directoryPath: string): Promise<void> {
-    try {
-      await fs.ensureDir(directoryPath);
-    } catch (error) {
-      throw new Error(unwrapErrorMessage(error));
-    }
-  }
-
-  async deleteDirectory(directoryPath: string): Promise<void> {
-    try {
-      await fs.remove(directoryPath);
     } catch (error) {
       throw new Error(unwrapErrorMessage(error));
     }
