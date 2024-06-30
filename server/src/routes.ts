a243181425af424911ba34d96ff8223c9c7577c2
Evgenii Mozharovskii <mozharovsky@live.com>
2024-06-30 23:55:03 +0400

    refactor(server): register routes asynchronously

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -4,14 +4,14 @@ import GitRouter from "./routers/gitRouter";
 import ProcessRouter from "./routers/processRouter";
 import TerminalRouter from "./routers/terminalRouter";
 
+import { EventEmitter } from "node:events";
 import { DirectoryService } from "./services/directoryService";
 import { FileService } from "./services/fileService";
-import { GitService } from "./services/gitService";
+import { createGitService } from "./services/gitServiceFactory";
 import { ProcessService } from "./services/processService";
 import { TerminalService } from "./services/terminalService";
 import { loadTemplate } from "./utils/loadTemplate";
 
-import { EventEmitter } from "node:events";
 import type { App, IAppRegistry } from "./app";
 import type { Config } from "./config";
 
@@ -21,33 +21,33 @@ export class AppRoutes {
     public config: Config,
   ) {}
 
-  public configureRoutes() {
-    this.appRegistry.registerRouter("/api/process", (app) => {
+  public async configureRoutes() {
+    await this.appRegistry.registerRouter("/api/process", (app) => {
       const template = loadTemplate(this.config);
       const processService = new ProcessService(template.commands, app.broadcaster);
       const processRouter = new ProcessRouter(processService);
       return processRouter.router;
     });
 
-    this.appRegistry.registerRouter("/api/file", () => {
+    await this.appRegistry.registerRouter("/api/files", () => {
       const fileService = new FileService();
       const fileRouter = new FileRouter(fileService);
       return fileRouter.router;
     });
 
-    this.appRegistry.registerRouter("/api/directory", () => {
+    await this.appRegistry.registerRouter("/api/directories", () => {
       const directoryService = new DirectoryService();
       const directoryRouter = new DirectoryRouter(directoryService);
       return directoryRouter.router;
     });
 
-    this.appRegistry.registerRouter("/api/git", () => {
-      const gitService = new GitService(this.config.repoPath);
+    await this.appRegistry.registerRouter("/api/git", async () => {
+      const gitService = await createGitService(this.config.repoPath);
       const gitRouter = new GitRouter(gitService);
       return gitRouter.router;
     });
 
-    this.appRegistry.registerRouter("/api/terminal", (app) => {
+    await this.appRegistry.registerRouter("/api/terminals", (app) => {
       const eventEmitter = new EventEmitter();
       const terminalService = new TerminalService(eventEmitter);
       const terminalRouter = new TerminalRouter(terminalService, app.broadcaster);
