8d9fc2c3af6907fd6902d0bd0eaa8e39212e2b7b
Evgenii Mozharovskii <mozharovsky@live.com>
2024-08-30 19:08:49 +0100

    feat(server): extend broadcaster with buffered input

--- server/src/utils/broadcaster.ts
+++ server/src/utils/broadcaster.ts
@@ -1,26 +1,49 @@
 import { WebSocket, type WebSocketServer } from "ws";
+import { BufferedStream } from "./stream";
 
-type BroadcastData = {
-  type: string;
-  data: string;
+type BroadcastEvent =
+  | "app:error" //
+  | "app:info"
+  | "app:exit"
+  | "terminal:stdout"
+  | "terminal:exit";
+
+type BroadcastMessage = {
+  id?: number;
+  event: BroadcastEvent;
+  params: Record<string, unknown>;
 };
 
 export interface IBroadcaster {
-  broadcast(data: BroadcastData): void;
+  broadcast(message: BroadcastMessage): void;
+  bufferedBroadcast(message: BroadcastMessage): void;
 }
 
 export class Broadcaster implements IBroadcaster {
   private wss: WebSocketServer | null = null;
+  private bufferedStream: BufferedStream<BroadcastMessage>;
 
-  constructor(wss: WebSocketServer) {
+  constructor(wss: WebSocketServer, cooldownPeriod = 500) {
     this.wss = wss;
+    this.bufferedStream = new BufferedStream(cooldownPeriod, this.flush.bind(this));
   }
 
-  broadcast(data: BroadcastData) {
+  broadcast(message: BroadcastMessage) {
     this.wss?.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
-        console.log(`Broadcasting message: ${JSON.stringify(data)}`);
-        client.send(JSON.stringify(data));
+        client.send(JSON.stringify([message]));
+      }
+    });
+  }
+
+  bufferedBroadcast(message: BroadcastMessage): void {
+    this.bufferedStream.add(message);
+  }
+
+  private flush(messages: BroadcastMessage[]): void {
+    this.wss?.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(messages));
       }
     });
   }
