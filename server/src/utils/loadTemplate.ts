f8a3f4e9a0f3c6e4d748bfd40ef6d8425152e4b1
Evgenii Mozharovskii <mozharovsky@live.com>
2024-09-09 17:23:08 +0100

    refactor(server/utils): try to guess the template path

--- server/src/utils/loadTemplate.ts
+++ server/src/utils/loadTemplate.ts
@@ -2,8 +2,9 @@ import fs from "fs";
 
 import { templateSchema, type Template } from "~/models/template";
 
-export function loadTemplate(templatePath: string): Template {
-  const templateContent = fs.readFileSync(templatePath, "utf-8");
+export function loadTemplate(workingDirectory: string, templatePath?: string): Template {
+  const path = templatePath ?? `${workingDirectory}/.glyphy/template.json`;
+  const templateContent = fs.readFileSync(path, "utf-8");
   const templateResult = templateSchema.safeParse(JSON.parse(templateContent));
 
   if (!templateResult.success) {
