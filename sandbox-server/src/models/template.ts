29242b76ed6b201074fe83bc80d0948a0feff801
Evgenii Mozharovskii <mozharovsky@live.com>
2024-09-09 19:09:09 +0100

    refactor(server): remove obsolete commands from template

--- sandbox-server/src/models/template.ts
+++ sandbox-server/src/models/template.ts
@@ -6,17 +6,9 @@ export const templateSchema = z
       enabled: z.boolean(),
       web: z.object({ command: z.string() }).optional(),
     }),
-    commands: z.object({
-      start: z.string().optional(),
-      lint: z.string().optional(),
-      format: z.string().optional(),
-      install: z.string().optional(),
-    }),
   })
   .passthrough();
 
 export type Template = z.infer<typeof templateSchema>;
 
 export type PreviewsTemplate = Template["previews"];
-
-export type CommandsTemplate = Template["commands"];
