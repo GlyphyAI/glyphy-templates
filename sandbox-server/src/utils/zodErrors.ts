0e0f0b75de9a586eb65765d1069e3a451288c863
Evgenii Mozharovskii <mozharovsky@live.com>
2024-09-20 18:58:45 +0100

    refactor(server/utils): unwrap error message with default error message

--- sandbox-server/src/utils/zodErrors.ts
+++ sandbox-server/src/utils/zodErrors.ts
@@ -4,12 +4,19 @@ export function concatZodErrors(error: ZodError, separator = "\n"): string {
   return error.errors.map((err) => `${err.path.join(".")} - ${err.message}`).join(separator);
 }
 
-export function unwrapErrorMessage(error: unknown): string {
+export function unwrapErrorMessage(error: unknown): string | undefined;
+export function unwrapErrorMessage(error: unknown, defaultErrorMessage: string): string;
+export function unwrapErrorMessage(
+  error: unknown,
+  defaultErrorMessage?: string,
+): string | undefined {
   if (error instanceof ZodError) {
     return concatZodErrors(error);
   } else if (error instanceof Error) {
     return error.message;
+  } else if (defaultErrorMessage !== undefined) {
+    return defaultErrorMessage;
   } else {
-    return "An unknown error occurred";
+    return undefined;
   }
 }
